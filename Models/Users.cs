27eac23be184b59d19343e18fd99cbb6bfd88959
allanDev9 <allanpenaguerra2003@gmail.com>
2025-12-09 01:55:43 -0600

    Modelo de usuarios

---
+++ Models/Users.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiFaceUnah.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public bool Active { get; set; }
+    }
+}
