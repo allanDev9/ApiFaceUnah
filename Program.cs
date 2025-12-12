bae3524a27d23529522a3c24bb5771796bbb1cab
allanDev9 <allanpenaguerra2003@gmail.com>
2025-12-12 03:06:50 -0600

    Contenerización con Docker y mejoras en configuración
    Se añaden archivos .dockerignore, Dockerfile y compose.yaml para contenerizar la aplicación .NET y facilitar su despliegue y desarrollo local con Docker y Docker Compose. Se documenta el proceso en README.Docker.md. En Program.cs se permite la configuración por variables de entorno y se refactoriza la verificación de conexión a la base de datos para mayor claridad y compatibilidad con entornos Docker.

--- Program.cs
+++ Program.cs
@@ -3,11 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddEnvironmentVariables();
+
 //Get connection string
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 //Add the DbContext and configure MYSQL 
-builder.Services.AddDbContext<DBContext>(options => 
+builder.Services.AddDbContext<DBContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
 );
 
@@ -33,15 +35,14 @@ app.MapControllers();
 //verify database connection on startup
 try
 {
-    using (var scope = app.Services.CreateScope())
-    {
-        var db = scope.ServiceProvider.GetRequiredService<DBContext>();
-        db.Database.CanConnect();
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<DBContext>();
+    db.Database.CanConnect();
 
-        Console.WriteLine("Conectado a Mysql correctamente");
-    }  
+    Console.WriteLine("Conectado a Mysql correctamente");
 
-}catch(Exception ex)
+}
+catch (Exception ex)
 {
     Console.WriteLine("Error al conectar a Mysql:" + ex.Message);
 }
